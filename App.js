83f296ac1ba4837a58fb48ee916f0b83a3264de1
Gregory Tsyapa <grinyatsy@gmail.com>
2022-11-22 17:29:02 +0200

    implemented post list

--- App.js
+++ App.js
@@ -1,16 +1,34 @@
-import React, {Component} from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Alert, View} from 'react-native';
+import axios from 'axios';
 import {Post} from './components/Post';
-export default class App extends Component {
-  render() {
-    return (
-      <View>
+
+export default function App() {
+  const [posts, setPosts] = useState();
+  const fetchPosts = () => {
+    axios
+      .get('https://637cd83716c1b892ebc0da42.mockapi.io/api/articles')
+      .then(({data}) => {
+        setPosts(data);
+      })
+      .catch(error => {
+        console.log(error);
+        Alert.alert('Error');
+      });
+  };
+
+  useEffect(fetchPosts, []);
+
+  return (
+    <View>
+      {posts?.map(post => (
         <Post
-          title="some"
-          imageUrl="https://www.positronx.io/wp-content/uploads/2020/02/react-native-150x150-1.jpg"
-          createdAt="22.11.2022"
+          key={post.id}
+          title={post.title}
+          imageUrl={post.imageUrl}
+          createdAt={post.createdAt}
         />
-      </View>
-    );
-  }
+      ))}
+    </View>
+  );
 }
